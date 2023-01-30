f20b920096c2cc46fa834e949f070d479144236a
siddiq <msiddiq1400@gmail.com>
2023-01-30 14:03:45 +0500

    post controller routes

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -24,14 +24,42 @@ export const createPost = async (req, res) => {
     }
 }
 
+/* READ */
 export const getFeedPosts = async (req, res) => {
-
+    try {
+        const post = await Post.find();
+        return res.status(200).json(post);
+    } catch (err) {
+        return res.status(404).json({msg: err.message})
+    }
 }
 
 export const getUserPosts = async (req, res) => {
-
+    try {
+        const {userId} = req.params
+        const post = await Post.findById(userId);
+        return res.status(200).json(post);
+    } catch (err) {
+        return res.status(404).json({msg: err.message})
+    }
 }
 
+/* UPDATE */
 export const likePost = async (req, res) => {
-
+    try {
+        const {id} = req.params
+        const {userId} = req.body
+        const post = await Post.findById(id)
+        const isLiked = post.likes.get(userId)
+        if(isLiked) {
+            post.likes.delete(userId)
+        } else {
+            post.likes.set(userId, true)
+        }
+        //new to true, to return the document after update is applied
+        const updatedPost = await Post.findByIdAndUpdate(id, {likes: post.likes}, {new: true});
+        return res.status(200).json(updatedPost);
+    } catch (err) {
+        return res.status(404).json({msg: err.message})
+    }
 }
