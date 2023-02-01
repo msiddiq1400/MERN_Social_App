f5325d9eb0e9c5d0288b1f84b87ef8c316910151
siddiq <msiddiq1400@gmail.com>
2023-02-01 05:34:32 +0500

    testing input data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/post.js'
 import { register } from "./controller/auth.js"
 import { verifyToken } from "./middleware/auth.js"
 import { createPost } from "./controller/posts.js"
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import {users, posts} from './data/index.js'
 
 /* CONFIGURATIONS */
 //below two for when using "type:module" inside package.json
@@ -62,5 +65,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port ${PORT}`))
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users)
+    // Post.insertMany(posts)
 }).catch((err) => console.log(`${err} did not connect`))
 
--- server/routes/post.js
+++ server/routes/post.js
@@ -5,7 +5,7 @@ import { getFeedPosts, getUserPosts, likePost } from "../controller/posts.js";
 const router = express.Router()
 
 /* READ */
-router.get('/', verifyToken, getFeedPosts)
+router.get('/', getFeedPosts)
 router.get("/:userId/posts", verifyToken, getUserPosts)
 
 /* UPDATE */
